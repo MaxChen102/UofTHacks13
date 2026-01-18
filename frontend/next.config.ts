c00959805ee988071a1eac263a430cde11705e9f
rachelkd <rachel05deng@gmail.com>
2026-01-17 21:50:51 -0500

    chore: let all images load for now
    will change back to ufs afterwards

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,18 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.figma.com",
-        pathname: "/api/mcp/asset/**",
-      },
-      {
-        protocol: "https",
-        hostname: "0zktu15h6i.ufs.sh",
-        pathname: "/f/**",
-      }
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "www.figma.com",
+    //     pathname: "/api/mcp/asset/**",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "0zktu15h6i.ufs.sh",
+    //     pathname: "/f/**",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "utfs.io",
+    //     pathname: "**",
+    //   },
+    // ],
+    unoptimized: true,
   },
 };
 
